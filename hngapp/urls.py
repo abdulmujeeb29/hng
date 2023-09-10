35659b61a207316ffafc10cce12ac786382d841d
Tiamiyu Abdulmujeeb Olamilekan <110623915+abdulmujeeb29@users.noreply.github.com>
2023-09-10 17:23:09 +0100

    Update urls.py

--- hngapp/urls.py
+++ hngapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns =[
-    path('api',views.get_info,name="get_info"),
+    path('api/',views.get_info,name="get_info"),
     path('',views.index,name="index"),
     
 ]
